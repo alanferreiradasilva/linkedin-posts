689897e8c66ca302f95bba6628a4fbe8c01db5f5
alanferreiradasilva <alanferreiradasilva@gmail.com>
2024-06-15 16:40:07 -0300

    Initial commit for NetCore.Caching

---
+++ backends/NetCore.CachingDemo/NetCore.CachingDemo.Api/Controllers/ContinentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.CachingDemo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContinentsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ backends/NetCore.CachingDemo/NetCore.CachingDemo.Api/Models/Continents/ContinentResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCore.CachingDemo.Api.Models.Continents
+{
+    public class ContinentResponseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int NumberOfCountries { get; set; }
+    }
+}
