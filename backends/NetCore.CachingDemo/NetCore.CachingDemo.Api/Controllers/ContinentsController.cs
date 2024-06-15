98e0a7364e05286582625e166037d209be2f3e1e
alanferreiradasilva <alanferreiradasilva@gmail.com>
2024-06-15 17:34:11 -0300

    .
    Commit Continent Seed data, memory cache inside ContinentsController and program.cs.

--- backends/NetCore.CachingDemo/NetCore.CachingDemo.Api/Controllers/ContinentsController.cs
+++ backends/NetCore.CachingDemo/NetCore.CachingDemo.Api/Controllers/ContinentsController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using NetCore.CachingDemo.Api.Models.Continents;
+using NetCore.CachingDemo.Api.Seed;
+using System.Net;
 
 namespace NetCore.CachingDemo.Api.Controllers
 {
@@ -6,10 +10,27 @@ namespace NetCore.CachingDemo.Api.Controllers
     [Route("[controller]")]
     public class ContinentsController : ControllerBase
     {
+        private readonly IMemoryCache _memoryCache;
+        private const string CONTINENT_KEY = "CONTINENTS";
+
+        public ContinentsController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            return Ok();
+            var entities = new List<ContinentResponseModel>();
+
+            if (!_memoryCache.TryGetValue(CONTINENT_KEY, out entities))
+            {
+                entities = ContinentSeed.GetAllContinents();
+
+                _memoryCache.Set(CONTINENT_KEY, entities);
+            }
+
+            return Ok(entities);
         }
     }
 }
---
+++ backends/NetCore.CachingDemo/NetCore.CachingDemo.Api/Seed/ContinentSeed.cs
@@ -0,0 +1,21 @@
+﻿using NetCore.CachingDemo.Api.Models.Continents;
+
+namespace NetCore.CachingDemo.Api.Seed
+{
+    public static class ContinentSeed
+    {
+        public static List<ContinentResponseModel> GetAllContinents()
+        {
+            return new List<ContinentResponseModel>()
+            {
+                new ContinentResponseModel { Id = 1, Name = "Africa", NumberOfCountries = 54 },
+                new ContinentResponseModel { Id = 2, Name = "Antarctica", NumberOfCountries = 0 },
+                new ContinentResponseModel { Id = 3, Name = "Asia", NumberOfCountries = 48 },
+                new ContinentResponseModel { Id = 4, Name = "Europe", NumberOfCountries = 44 },
+                new ContinentResponseModel { Id = 5, Name = "North America", NumberOfCountries = 23 },
+                new ContinentResponseModel { Id = 6, Name = "South America", NumberOfCountries = 12 },
+                new ContinentResponseModel { Id = 7, Name = "Oceania", NumberOfCountries = 14 }
+            };
+        }
+    }
+}
